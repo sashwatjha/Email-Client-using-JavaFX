7f4db8f57b4fed853be2d8316dad650e28164239
Sashwat <sashwatjha666@gmail.com>
2022-04-26 14:34:43 +0530

    Java

---
+++ src/com/files/Launcher.java
@@ -0,0 +1,47 @@
+package com.files;
+
+import com.files.controller.persistence.PersistenceAccess;
+import com.files.controller.persistence.ValidAccount;
+import com.files.controller.services.LoginService;
+import com.files.model.EmailAccount;
+import com.files.view.ViewFactory;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    private PersistenceAccess persistenceAccess = new PersistenceAccess();
+    private EmailManager emailManager = new EmailManager();
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        ViewFactory viewFactory = new ViewFactory(emailManager);
+        List<ValidAccount> validAccountList = persistenceAccess.loadFromPersistence();
+        if(validAccountList.size() > 0) {
+            viewFactory.showMainWindow();
+            for (ValidAccount validAccount: validAccountList){
+                EmailAccount emailAccount = new EmailAccount(validAccount.getAddress(), validAccount.getPassword());
+                LoginService loginService = new LoginService(emailAccount, emailManager);
+                loginService.start();
+            }
+        } else {
+            viewFactory.showLoginWindow();
+        }
+    }
+
+
+    @Override
+    public void stop() throws Exception {
+        List<ValidAccount> validAccountList = new ArrayList<ValidAccount>();
+        for (EmailAccount emailAccount: emailManager.getEmailAccounts()){
+            validAccountList.add(new ValidAccount(emailAccount.getAddress(), emailAccount.getPassword()));
+        }
+        persistenceAccess.saveToPersistence(validAccountList);
+    }
+}
